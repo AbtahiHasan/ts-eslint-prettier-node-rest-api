a24e81743b2914721639ff29a793ab89354a20cd
AbtahiHasan <anikidpersonal@gmail.com>
2023-11-22 12:06:23 +0600

    initial setupdone

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+export const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = req.query.a;
+
+  res.send(a);
+});
+
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  try {
+    next(`Route ${req.originalUrl} not found`);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+//     try {
+//         res.status(500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     } catch (error) {
+//         console.log(error)
+//     }
+// })
