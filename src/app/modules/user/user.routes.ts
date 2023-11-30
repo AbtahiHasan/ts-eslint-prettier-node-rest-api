405645c72507f0619e13f5d7a6f27a57c32180b7
AbtahiHasan <anikidpersonal@gmail.com>
2023-11-30 13:35:57 +0600

    request validator warning fixed

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -2,8 +2,8 @@ import express from 'express';
 import userController from './user.controller';
 import validateRequest from '../../middleware/validateRequest';
 import studentValidationSchema from '../student/student.validation';
-const userRouter = express.Router();
 
+const userRouter = express.Router();
 userRouter.post(
   '/create-student',
   validateRequest(studentValidationSchema.createStudentValidationSchema),
