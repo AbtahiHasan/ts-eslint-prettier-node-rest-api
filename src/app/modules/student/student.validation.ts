41f08e970f593bdf0fb795a8bb06db7af931ced1
AbtahiHasan <anikidpersonal@gmail.com>
2023-11-30 13:27:02 +0600

    create student validation done

--- src/app/modules/student/student.validation.ts
+++ src/app/modules/student/student.validation.ts
@@ -24,18 +24,21 @@ const localGuardianValidationSchema = z.object({
 
 const createStudentValidationSchema = z.object({
   body: z.object({
-    name: userNameValidationSchema,
-    gender: z.enum(['male', 'female']),
-    dateOfBirth: z.string(),
-    email: z.string().email(),
-    contactNo: z.string(),
-    emergencyContactNo: z.string(),
-    presentAddress: z.string(),
-    permanentAddress: z.string(),
-    guardian: guardianValidationSchema,
-    localGuardian: localGuardianValidationSchema,
-    profileImage: z.string(),
-    academicDepartment: z.string(),
+    password: z.string().optional(),
+    student: z.object({
+      name: userNameValidationSchema,
+      gender: z.enum(['male', 'female']),
+      dateOfBirth: z.string(),
+      email: z.string().email(),
+      contactNo: z.string(),
+      emergencyContactNo: z.string(),
+      presentAddress: z.string(),
+      permanentAddress: z.string(),
+      guardian: guardianValidationSchema,
+      localGuardian: localGuardianValidationSchema,
+      profileImage: z.string(),
+      academicDepartment: z.string(),
+    }),
   }),
 });
 
--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 import userController from './user.controller';
+import validateRequest from '../../middleware/validateRequest';
+import studentValidationSchema from '../student/student.validation';
 const userRouter = express.Router();
 
-userRouter.post('/create-student', userController.createStudent);
+userRouter.post(
+  '/create-student',
+  validateRequest(studentValidationSchema.createStudentValidationSchema),
+  userController.createStudent,
+);
 
 export default userRouter;
